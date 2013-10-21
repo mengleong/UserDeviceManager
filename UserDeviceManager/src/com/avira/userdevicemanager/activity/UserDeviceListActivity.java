3bbae3abf4573c4a968956a79e776f1340fc334c
unknown <meng-leong.kwan@WSKL13371.ad.avira.com>
2013-10-21 14:13:08 +0800

    commit to new repo

---
+++ UserDeviceManager/src/com/avira/userdevicemanager/activity/UserDeviceListActivity.java
@@ -0,0 +1,5 @@
+package com.avira.userdevicemanager.activity;
+
+public class UserDeviceListActivity {
+
+}
---
+++ UserDeviceManager/src/com/avira/userdevicemanager/view/DevicesListFragment.java
@@ -0,0 +1,7 @@
+package com.avira.userdevicemanager.view;
+
+import android.support.v4.app.ListFragment;
+
+public class DevicesListFragment extends ListFragment{
+
+}
